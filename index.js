b97b8ce3fcad6eb38fd1f79077fcaeee26025441
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-13 22:05:05 +0300

    Completed assiignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + ' !';
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + ' GRACE HOPPER ' +  ' !';
-const shortGreeting = 'Welcome, ' + ' G ' + ' !';
-
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+const ecitedWelcomeMessage = welcomeMessage.toUpperCase();
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${GRACE HOPPER}!';
+const shortGreeting = 'Welcome, ${G}!';
